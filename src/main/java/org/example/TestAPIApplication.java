c23c93edb1bd7ebe68658dedef34e941597ff504
baliram-bhande-id <deepalisakhare26@gmail.com>
2025-07-15 17:42:47 +0530

    Repeated char in string

--- src/main/java/org/example/TestAPIApplication.java
+++ src/main/java/org/example/TestAPIApplication.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Arrays;
+
+
+/**
+ * Array Question: Find the missing number in an array from 1 to N
+ * Example: [1, 2, 4, 5, 6] → Output: 3
+ */
 public class TestAPIApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] input = {1, 2, 4, 5, 6};
+
+        int n = input.length + 1; // Total numbers should be 6 (1 to 6)
+        int expectedSum = n * (n + 1) / 2; // Sum from 1 to n
+
+        int actualSum = Arrays.stream(input).sum();
+
+        int missing = expectedSum - actualSum;
+
+        System.out.println("Missing number is: " + missing);
     }
 }
